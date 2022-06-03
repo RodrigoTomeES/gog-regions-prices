9742636c15a64d3b28c9ef422c01bd6d560e8b02
RodrigoTomeES <rotome@unirioja.es>
2022-06-04 00:59:24 +0200

    chore(extract): prices are now extracted from all countries

--- src/extract.js
+++ src/extract.js
@@ -1,54 +1,66 @@
-const request = require("request")
-const fs = require("fs")
+const request = require("request");
+const fs = require("fs");
+const regions = require("./regions");
 
-const gamesDB = `${__dirname}/../public/games.json`
-const salesDB = `${__dirname}/../public/sales.json`
-const lastUpdate = `${__dirname}/../public/lastUpdate.json`
-const GRACE_TIME = 750
+const gamesDB = `${__dirname}/../public/games.json`;
+const salesDB = `${__dirname}/../public/sales.json`;
+const lastUpdate = `${__dirname}/../public/lastUpdate.json`;
+const GRACE_TIME = 750;
 
-updateGOGGames()
+updateGOGGames();
 
 async function updateGOGGames(maxPages = undefined) {
-  console.info(`Retrieving information from GOG with Max Pages set to ${maxPages}`)
-  console.info(' Reading DB...')
+  console.info(
+    `Retrieving information from GOG with Max Pages set to ${maxPages}`
+  );
+  console.info(" Reading DB...");
 
   let gamesJSON = null;
 
   try {
     gamesJSON = fs.readFileSync(gamesDB, "utf-8");
   } catch (err) {
-    console.error('  DB doesn\'t exist, creating...')
+    console.error("  DB doesn't exist, creating...");
   }
 
-  console.info(" Obtaining games prices...")
-  const regions = ["ES", "RU", "UA", "AR", "TR", "CN", "NZ", "PL"]
-  const games = await Promise.all(regions.map(async (country) => await getGames(country, maxPages)))
+  console.info(" Obtaining games prices...");
+  const games = await Promise.all(
+    regions.map(async (country) => await getGames(country, maxPages))
+  );
 
-  console.info(" Putting everything together...")
-  const [newGames, newSales] = joinPrices(...games)
+  console.info(" Putting everything together...");
+  const [newGames, newSales] = joinPrices(...games);
 
-  if (gamesJSON === null || gamesJSON.localeCompare(JSON.stringify(newGames)) != 0) {
-    console.info(" SOME DIFFERENCES FOUND, writing to disk...")
-    fs.writeFileSync(gamesDB, JSON.stringify(newGames))
-    fs.writeFileSync(salesDB, JSON.stringify(newSales))
-    fs.writeFileSync(lastUpdate, JSON.stringify({ last: new Date().toISOString() }))
+  if (
+    gamesJSON === null ||
+    gamesJSON.localeCompare(JSON.stringify(newGames)) != 0
+  ) {
+    console.info(" SOME DIFFERENCES FOUND, writing to disk...");
+    fs.writeFileSync(gamesDB, JSON.stringify(newGames));
+    fs.writeFileSync(salesDB, JSON.stringify(newSales));
+    fs.writeFileSync(
+      lastUpdate,
+      JSON.stringify({ last: new Date().toISOString() })
+    );
   } else {
-    console.info(" NOTHING CHANGED SINCE LAST RUN, aborting...")
+    console.info(" NOTHING CHANGED SINCE LAST RUN, aborting...");
   }
 }
 
 function getGames(country, maxPages) {
   return new Promise(async (resolve, _) => {
-    let page = 1, totalPages = maxPages, games = new Map()
+    let page = 1,
+      totalPages = maxPages,
+      games = new Map();
 
     do {
-      if (page % 10 == 1) console.info(`  ${country} - Page ${page}`)
-      const result = await getGOGData(country, page)
-      totalPages = totalPages || result.totalPages
+      if (page % 10 == 1) console.info(`  ${country} - Page ${page}`);
+      const result = await getGOGData(country, page);
+      totalPages = totalPages || result.totalPages;
 
       result.products
-        .filter(product => product.isGame)
-        .forEach(product => {
+        .filter((product) => product.isGame)
+        .forEach((product) => {
           const mapped = {
             id: product.id,
             title: product.title,
@@ -56,55 +68,55 @@ function getGames(country, maxPages) {
             url: product.url,
             image: product.image,
             price: product.price.baseAmount,
-            country: country
-          }
+            country: country,
+          };
 
           if (product.price.baseAmount != product.price.finalAmount) {
-            mapped.sale = product.price.finalAmount
+            mapped.sale = product.price.finalAmount;
           }
 
           games.set(product.id, mapped);
-        })
+        });
 
-      await sleep(GRACE_TIME)
-    } while (page++ != totalPages)
+      await sleep(GRACE_TIME);
+    } while (page++ != totalPages);
 
-    resolve(games)
-  })
-
+    resolve(games);
+  });
 }
 
 function getGOGData(country, page) {
   return new Promise((resolve, reject) => {
     request(options(country, page), (error, response, body) => {
       if (error || response.statusCode == 500) {
-        reject(error)
+        reject(error);
       } else if (!error && response.statusCode == 200 && IsJsonString(body)) {
-        resolve(JSON.parse(body))
+        resolve(JSON.parse(body));
       } else {
         resolve({
           totalPages: 1,
-          products: []
-        })
+          products: [],
+        });
       }
-    })
-  })
+    });
+  });
 }
 
-const orderByID = (a, b) => a.id - b.id
+const orderByID = (a, b) => a.id - b.id;
+const orderByPrice = (a, b) => a.price - b.price;
 
 function options(country = "RU", page = 1) {
   return {
     url: `https://www.gog.com/games/ajax/filtered?sort=title&page=${page}`,
     headers: {
-      Cookie: 'gog_lc=' + country + '_USD_en-US; path=/'
-    }
-  }
+      Cookie: "gog_lc=" + country + "_USD_en-US; path=/",
+    },
+  };
 }
 
 function joinPrices(first, ...args) {
-  const newGames = []
-  const newSales = []
+  const newGames = [];
+  const newSales = [];
 
   for (const [key, game] of first) {
     const info = {
@@ -114,42 +126,63 @@ function joinPrices(first, ...args) {
       url: game.url,
       image: game.image,
       country: {},
-      price: {}
-    }
+      contriesWithDiferentPrices: [],
+      countryByPrice: [],
+      price: {},
+    };
 
-    info.price[game.country] = game.price
-    info.country[game.country] = game.country
+    info.price[game.country] = game.price;
+    info.country[game.country] = game.country;
+    info.contriesWithDiferentPrices.push(game.country);
 
     if (game.sale != undefined) {
-      info.sale = {}
-      info.sale[game.country] = game.sale
+      info.sale = {};
+      info.sale[game.country] = game.sale;
     }
 
-    args.forEach(mapGamesOfCountry => {
+    args.forEach((mapGamesOfCountry) => {
       if (mapGamesOfCountry.has(key)) {
-        const gameOtherCountry = mapGamesOfCountry.get(key)
-        info.price[gameOtherCountry.country] = gameOtherCountry.price
-        info.country[gameOtherCountry.country] = gameOtherCountry.country
+        const gameOtherCountry = mapGamesOfCountry.get(key);
+        info.price[gameOtherCountry.country] = gameOtherCountry.price;
+        info.country[gameOtherCountry.country] = gameOtherCountry.country;
 
         if (gameOtherCountry.sale != undefined) {
-          info.sale[gameOtherCountry.country] = gameOtherCountry.sale
+          info.sale[gameOtherCountry.country] = gameOtherCountry.sale;
         }
+
+        let shouldAdd = true;
+
+        info.contriesWithDiferentPrices.every((country) => {
+          if (info.price[country] === gameOtherCountry.price) {
+            shouldAdd = false;
+            return false;
+          }
+
+          return true;
+        });
+
+        if (shouldAdd)
+          info.contriesWithDiferentPrices.push(gameOtherCountry.country);
       }
-    })
+    });
 
-    newGames.push(info)
+    info.contriesWithDiferentPrices.forEach((country) =>
+      info.countryByPrice.push({ price: info.price[country], country: country })
+    );
+    info.countryByPrice.sort(orderByPrice);
 
-    if (game.sale) newSales.push(info)
+    newGames.push(info);
+
+    if (game.sale) newSales.push(info);
   }
 
-
-  return [newGames.sort(orderByID), newSales.sort(orderByID)]
+  return [newGames.sort(orderByID), newSales.sort(orderByID)];
 }
 
 function sleep(ms) {
-  return new Promise(resolve => {
-    setTimeout(resolve, ms)
-  })
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
 
 function IsJsonString(str) {
